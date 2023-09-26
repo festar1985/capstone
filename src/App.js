28361b580255c063fece1f05cb5eabf0b65e3dd3
Georgi Konstantinov <georgi.k.konstantinov@gmail.com>
2023-09-26 22:25:04 +0200

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Header>
+        <Nav></Nav>
+      </Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+
+function Footer() {
+  return (
+    <>
+      <img src={logo} alt="Little Lemon Logo" />
+      <Nav></Nav>
+      <p>Contact</p>
+      <p>Social Media Links</p>
+    </>
+  );
+}
+
+export default Footer;
